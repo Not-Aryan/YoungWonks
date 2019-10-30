9809d3c9912b605af7923f5716429a25a3f30c4d
Not-Aryan <45083845+Not-Aryan@users.noreply.github.com>
2019-10-29 18:54:49 -0700

    Add files via upload

---
+++ Algothrims Review.py
@@ -0,0 +1,28 @@
+def linear(lis, o):
+    for i in range(0, len(lis)):
+        if lis[i] == o:
+            print(lis[i])
+
+lis = [1,2,3,4,5,6]
+##linear(lis, 2)
+
+    
+def binary_search(item_list,item):
+	first = 0
+	last = len(item_list)-1
+	found = False
+	while( first<=last and not found):
+		mid = (first + last)//2
+		if item_list[mid] == item :
+			found = True
+		else:
+			if item < item_list[mid]:
+				last = mid - 1
+			else:
+				first = mid + 1	
+	return found
+	
+print(binary_search([1,2,3,5,8], 6))
+print(binary_search([1,2,3,5,8], 5))
+
+
